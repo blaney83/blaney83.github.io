a27ee77f41f5233e595e734b87490b61b7f53599
Blaney83 <professionallaney@gmail.com>
2019-01-11 18:23:00 -0700

    Data update.

--- src/components/Charts/Scripting.js
+++ src/components/Charts/Scripting.js
@@ -3,9 +3,9 @@ const Chartist = require("chartist")
 
 var scriptChart = {
     data: {
-        labels: ['JS-ES7', 'React', 'Redux', 'Hooks', 'React w/\nMaterial-UI', 'jQuery'],
+        labels: ['JavaFX', 'JS-ES7', 'React', 'Redux', 'Hooks', 'React w/\nMaterial-UI', 'jQuery'],
         series: [
-            [9.2, 8.5, 8.5, 9, 9, 8.5],
+            [6, 9.2, 8.5, 8.5, 9, 9, 8.5],
         ]
     },
     options: {
